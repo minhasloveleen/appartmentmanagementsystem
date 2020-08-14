b5993ccefaa481c6d07d2afa84cc6f64398c5a8e
Harikrishna <harikrishnasabu@gmail.com>
2020-08-14 10:43:27 -0400

    Design of Appartment listing HomePage and assosiated images

---
+++ src/home.php
@@ -0,0 +1,241 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<title>Find Your Appartment</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", Arial, Helvetica, sans-serif}
+</style>
+<body class="w3-light-grey">
+
+<!-- Navigation Bar -->
+<div class="w3-bar w3-white w3-large">
+  <a href="#" class="w3-bar-item w3-button w3-red w3-mobile"><i class="fa fa-bed w3-margin-right"></i>My Appartment</a>
+
+  <a href="#" class="w3-bar-item w3-button w3-right w3-light-grey w3-mobile">Signin</a>
+  <a href="#" class="w3-bar-item w3-button w3-right w3-light-grey w3-mobile">Signup</a>
+</div>
+<!-- Header -->
+<header class="w3-display-container w3-content" style="max-width:1500px;">
+  <img class="w3-image" src="images/hotel.jpg" alt="The Hotel" style="min-width:1000px" width="1500" height="800">
+  <div class="w3-display-left w3-padding w3-col l6 m8">
+    <div class="w3-container w3-red">
+      <h2><i class="fa fa-bed w3-margin-right"></i>Let's Find Your Appartment today!</h2>
+    </div>
+</header>
+
+<!-- Page content -->
+<div class="w3-content" style="max-width:1532px;">
+
+  <div class="w3-bar w3-white w3-large">
+  <a href="#rooms" class="w3-bar-item w3-button w3-mobile">5 1/2</a>
+  <a href="#about" class="w3-bar-item w3-button w3-mobile">4 1/2</a>
+  <a href="#contact" class="w3-bar-item w3-button w3-mobile">3 1/2</a>
+  </div>
+  
+  
+
+  <div class="w3-row-padding w3-padding-16">
+    <div class="w3-third w3-margin-bottom">
+      <img src="images/room_single.jpg" alt="Norway" style="width:100%">
+      <div class="w3-container w3-white">
+        <h3>Fully Furnished</h3>
+        <h6 class="w3-opacity">From $99</h6>
+        <p>All Included</p>
+        <p>5<sup>1/2</sup></p>
+        <p class="w3-large"><i class="fa fa-bath"></i> <i class="fa fa-phone"></i> <i class="fa fa-wifi"></i></p>
+        <button class="w3-button w3-block w3-black w3-margin-bottom">Details</button>
+      </div>
+    </div>
+    <div class="w3-third w3-margin-bottom">
+      <img src="images/room_double.jpg" alt="Norway" style="width:100%">
+      <div class="w3-container w3-white">
+        <h3> Premium Appartment</h3>
+        <h6 class="w3-opacity">From $149</h6>
+        <p>All Included</p>
+        <p>3<sup>1/2</sup></p>
+        <p class="w3-large"><i class="fa fa-bath"></i> <i class="fa fa-phone"></i> <i class="fa fa-wifi"></i> <i class="fa fa-tv"></i></p>
+        <button class="w3-button w3-block w3-black w3-margin-bottom">Details</button>
+      </div>
+    </div>
+    <div class="w3-third w3-margin-bottom">
+      <img src="images/room_deluxe.jpg" alt="Norway" style="width:100%">
+      <div class="w3-container w3-white">
+        <h3>Fully Furnished</h3>
+        <h6 class="w3-opacity">From $199</h6>
+        <p>All Included</p>
+        <p>4<sup>1/2</sup></p>
+        <p class="w3-large"><i class="fa fa-bath"></i> <i class="fa fa-phone"></i> <i class="fa fa-wifi"></i> <i class="fa fa-tv"></i> <i class="fa fa-glass"></i> <i class="fa fa-cutlery"></i></p>
+        <button class="w3-button w3-block w3-black w3-margin-bottom">Details</button>
+      </div>
+    </div>
+    <div class="w3-third w3-margin-bottom">
+      <img src="images/room_single.jpg" alt="Norway" style="width:100%">
+      <div class="w3-container w3-white">
+        <h3>Fully Furnished</h3>
+        <h6 class="w3-opacity">From $99</h6>
+        <p>All Included</p>
+        <p>5<sup>1/2</sup></p>
+        <p class="w3-large"><i class="fa fa-bath"></i> <i class="fa fa-phone"></i> <i class="fa fa-wifi"></i></p>
+        <button class="w3-button w3-block w3-black w3-margin-bottom">Details</button>
+      </div>
+    </div>
+    <div class="w3-third w3-margin-bottom">
+      <img src="images/room_single.jpg" alt="Norway" style="width:100%">
+      <div class="w3-container w3-white">
+        <h3>Fully Furnished</h3>
+        <h6 class="w3-opacity">From $99</h6>
+        <p>All Included</p>
+        <p>5<sup>1/2</sup></p>
+        <p class="w3-large"><i class="fa fa-bath"></i> <i class="fa fa-phone"></i> <i class="fa fa-wifi"></i></p>
+        <button class="w3-button w3-block w3-black w3-margin-bottom">Details</button>
+      </div>
+    </div>
+    <div class="w3-third w3-margin-bottom">
+      <img src="images/room_single.jpg" alt="Norway" style="width:100%">
+      <div class="w3-container w3-white">
+        <h3>Fully Furnished</h3>
+        <h6 class="w3-opacity">From $99</h6>
+        <p>All Included</p>
+        <p>5<sup>1/2</sup></p>
+        <p class="w3-large"><i class="fa fa-bath"></i> <i class="fa fa-phone"></i> <i class="fa fa-wifi"></i></p>
+        <button class="w3-button w3-block w3-black w3-margin-bottom">Details</button>
+      </div>
+    </div>
+    <div class="w3-third w3-margin-bottom">
+      <img src="images/room_double.jpg" alt="Norway" style="width:100%">
+      <div class="w3-container w3-white">
+        <h3>Double Room</h3>
+        <h6 class="w3-opacity">From $149</h6>
+        <p>Queen-size bed</p>
+        <p>5<sup>1/2</sup></p>
+        <p class="w3-large"><i class="fa fa-bath"></i> <i class="fa fa-phone"></i> <i class="fa fa-wifi"></i> <i class="fa fa-tv"></i></p>
+        <button class="w3-button w3-block w3-black w3-margin-bottom">Details</button>
+      </div>
+    </div>
+    <div class="w3-third w3-margin-bottom">
+      <img src="images/room_single.jpg" alt="Norway" style="width:100%">
+      <div class="w3-container w3-white">
+        <h3>Fully Furnished</h3>
+        <h6 class="w3-opacity">From $99</h6>
+        <p>All Included</p>
+        <p>5<sup>1/2</sup></p>
+        <p class="w3-large"><i class="fa fa-bath"></i> <i class="fa fa-phone"></i> <i class="fa fa-wifi"></i></p>
+        <button class="w3-button w3-block w3-black w3-margin-bottom">Details</button>
+      </div>
+    </div>
+    <div class="w3-third w3-margin-bottom">
+      <img src="images/room_single.jpg" alt="Norway" style="width:100%">
+      <div class="w3-container w3-white">
+        <h3>Fully Furnished</h3>
+        <h6 class="w3-opacity">From $99</h6>
+        <p>All Included</p>
+        <p>5<sup>1/2</sup></p>
+        <p class="w3-large"><i class="fa fa-bath"></i> <i class="fa fa-phone"></i> <i class="fa fa-wifi"></i></p>
+        <button class="w3-button w3-block w3-black w3-margin-bottom">Details</button>
+      </div>
+    </div>
+    <div class="w3-third w3-margin-bottom">
+      <img src="images/room_double.jpg" alt="Norway" style="width:100%">
+      <div class="w3-container w3-white">
+        <h3>Double Room</h3>
+        <h6 class="w3-opacity">From $149</h6>
+        <p>Queen-size bed</p>
+        <p>3<sup>1/2</sup></p>
+        <p class="w3-large"><i class="fa fa-bath"></i> <i class="fa fa-phone"></i> <i class="fa fa-wifi"></i> <i class="fa fa-tv"></i></p>
+        <button class="w3-button w3-block w3-black w3-margin-bottom">Details</button>
+      </div>
+    </div>
+    <div class="w3-third w3-margin-bottom">
+      <img src="images/room_single.jpg" alt="Norway" style="width:100%">
+      <div class="w3-container w3-white">
+        <h3>Fully Furnished</h3>
+        <h6 class="w3-opacity">From $99</h6>
+        <p>All Included</p>
+        <p>5<sup>1/2</sup></p>
+        <p class="w3-large"><i class="fa fa-bath"></i> <i class="fa fa-phone"></i> <i class="fa fa-wifi"></i></p>
+        <button class="w3-button w3-block w3-black w3-margin-bottom">Details</button>
+      </div>
+    </div>
+    <div class="w3-third w3-margin-bottom">
+      <img src="images/room_single.jpg" alt="Norway" style="width:100%">
+      <div class="w3-container w3-white">
+        <h3>Fully Furnished</h3>
+        <h6 class="w3-opacity">From $99</h6>
+        <p>All Included</p>
+        <p>3<sup>1/2</sup></p>
+        <p class="w3-large"><i class="fa fa-bath"></i> <i class="fa fa-phone"></i> <i class="fa fa-wifi"></i></p>
+        <button class="w3-button w3-block w3-black w3-margin-bottom">Details</button>
+      </div>
+    </div>
+    <div class="w3-third w3-margin-bottom">
+      <img src="images/room_double.jpg" alt="Norway" style="width:100%">
+      <div class="w3-container w3-white">
+        <h3>Double Room</h3>
+        <h6 class="w3-opacity">From $149</h6>
+        <p>Queen-size bed</p>
+        <p>4<sup>1/2</sup></p>
+        <p class="w3-large"><i class="fa fa-bath"></i> <i class="fa fa-phone"></i> <i class="fa fa-wifi"></i> <i class="fa fa-tv"></i></p>
+        <button class="w3-button w3-block w3-black w3-margin-bottom">Details</button>
+      </div>
+    </div>
+    <div class="w3-third w3-margin-bottom">
+      <img src="images/room_single.jpg" alt="Norway" style="width:100%">
+      <div class="w3-container w3-white">
+        <h3>Fully Furnished</h3>
+        <h6 class="w3-opacity">From $99</h6>
+        <p>All Included</p>
+        <p>3<sup>1/2</sup></p>
+        <p class="w3-large"><i class="fa fa-bath"></i> <i class="fa fa-phone"></i> <i class="fa fa-wifi"></i></p>
+        <button class="w3-button w3-block w3-black w3-margin-bottom">Details</button>
+      </div>
+    </div>
+    <div class="w3-third w3-margin-bottom">
+      <img src="images/room_single.jpg" alt="Norway" style="width:100%">
+      <div class="w3-container w3-white">
+        <h3>Fully Furnished</h3>
+        <h6 class="w3-opacity">From $99</h6>
+        <p>All Included</p>
+        <p>5<sup>1/2</sup></p>
+        <p class="w3-large"><i class="fa fa-bath"></i> <i class="fa fa-phone"></i> <i class="fa fa-wifi"></i></p>
+        <button class="w3-button w3-block w3-black w3-margin-bottom">Details</button>
+      </div>
+    </div>
+  </div>
+
+ 
+  
+  <div class="w3-row-padding w3-large w3-center" style="margin:32px 0">
+    <div class="w3-third"><i class="fa fa-map-marker w3-text-red"></i> 123 Montreal, Quebec, CA</div>
+    <div class="w3-third"><i class="fa fa-phone w3-text-red"></i> Phone: +1 1234567890</div>
+    <div class="w3-third"><i class="fa fa-envelope w3-text-red"></i> Email: mail@mail.com</div>
+  </div>
+
+  <div class="w3-container" id="contact">
+    <h2>Contact</h2>
+    <p>If you have any questions, do not hesitate to ask them.</p>
+    <i class="fa fa-map-marker w3-text-red" style="width:30px"></i> 123 Montreal, Quebec, CA<br>
+    <i class="fa fa-phone w3-text-red" style="width:30px"></i> Phone: +1 1234567890<br>
+    <i class="fa fa-envelope w3-text-red" style="width:30px"> </i> Email: mail@mail.com<br>
+    <form action="/action_page.php" target="_blank">
+      <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Name" required name="Name"></p>
+      <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Email" required name="Email"></p>
+      <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Message" required name="Message"></p>
+      <p><button class="w3-button w3-black w3-padding-large" type="submit">SEND MESSAGE</button></p>
+    </form>
+  </div>
+
+<!-- End page content -->
+</div>
+
+
+
+</body>
+</html>
