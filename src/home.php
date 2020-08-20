05380c299b19637ea97b2d2cbbe436800b8a2361
Loveleen Kaur Loveleen Kaur <llovelee@isimtl.local>
2020-08-20 00:49:32 -0400

    Revert "Change in home page add project style header"
    This reverts commit cd7d9ea82e301c0c00cb5900404af9158c85b00e.

--- src/home.php
+++ src/home.php
@@ -10,24 +10,31 @@
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link type="text/css" rel="stylesheet" href="css/styleindex.css" />
 <style>
 body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", Arial, Helvetica, sans-serif}
 </style>
 <body class="w3-light-grey">
 
 <!-- Navigation Bar -->
-<header id="top">
-	<img  src="images/logo.png" alt="logo" style="margin-top:10px;"/>
-    <h1 >Delta <span style="color:#54514d">Management<span></h1>
-    <p> Hi Username <a href="login.php">   Signup</a></p>
+<div class="w3-bar w3-white w3-large">
+  <a href="#" class="w3-bar-item w3-button w3-red w3-mobile"><i class="fa fa-bed w3-margin-right"></i>My Appartment</a>
 
+  <a href="#" class="w3-bar-item w3-button w3-right w3-light-grey w3-mobile">Signin</a>
+  <a href="#" class="w3-bar-item w3-button w3-right w3-light-grey w3-mobile">Signup</a>
+</div>
+<!-- Header -->
+<header class="w3-display-container w3-content" style="max-width:1500px;">
+  <img class="w3-image" src="images/hotel.jpg" alt="The Hotel" style="min-width:1000px" width="1500" height="800">
+  <div class="w3-display-left w3-padding w3-col l6 m8">
+    <div class="w3-container w3-red">
+      <h2><i class="fa fa-bed w3-margin-right"></i>Let's Find Your Appartment today!</h2>
+    </div>
 </header>
 
+<!-- Page content -->
+<div class="w3-content" style="max-width:1532px;">
+
   <div class="w3-bar w3-white w3-large">
-  <div>
-  <img class="w3-image" src="images/hotel.jpg" alt="The Hotel" style="min-width:1000px" width="1500" height="800">
-  </div>
   <a href="#rooms" class="w3-bar-item w3-button w3-mobile">5 1/2</a>
   <a href="#about" class="w3-bar-item w3-button w3-mobile">4 1/2</a>
   <a href="#contact" class="w3-bar-item w3-button w3-mobile">3 1/2</a>
@@ -229,6 +236,6 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", Arial, Helvetica, sans-serif}
 </div>
 
 
-<?php 
-    include_once("includes/footer.php");
-?>
+
+</body>
+</html>
