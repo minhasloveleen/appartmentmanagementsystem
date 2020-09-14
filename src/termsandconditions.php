f4e569cea280b5cc34e37af91263ba9d3ad23cad
Harikrishna Sabu <61350098+harikrishnasabu@users.noreply.github.com>
2020-09-14 13:49:59 -0400

    Create termsandconditions.php
    Terms and Conditions Page

---
+++ src/termsandconditions.php
@@ -0,0 +1,100 @@
+<?php 
+    include 'includes/database.php';
+    include_once("includes/header.php");
+?>
+<?php
+
+?>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<div class="main">
+    <?php 
+        include_once("includes/adminmenu.php");
+    ?>
+    <div class="section" style="padding-top:0px">
+    <h1>Terms and Conditions -Delta Management </h1>
+        <?php
+        $DispDiv ="display:none;";
+
+       
+
+    ?>
+       
+       <div class="w3-container" id="contact">
+ 
+   
+      
+
+<h3>Last updated: (September 14, 2020)</h3>
+
+<p>Please read these Terms and Conditions carefully before using the deltamanagement website operated by Delta Management Systems.
+
+Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service.
+
+By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service.
+</p>
+<h3>
+Purchases
+</h3>
+<p>
+If you wish to purchase any product or service made available through the Service ("Purchase"), you may be asked to supply certain information relevant to your Purchase including, without limitation, your …
+
+The Purchases section is for businesses that sell online (physical or digital). For the full disclosure section, create your own Terms and Conditions.
+</p>
+<h3>
+Subscriptions
+</h3>
+<p>
+Some parts of the Service are billed on a subscription basis ("Subscription(s)"). You will be billed in advance on a recurring ...
+
+The Subscriptions section is for SaaS businesses. For the full disclosure section, create your own Terms and Conditions.
+
+
+</p>
+<h3>
+Content
+</h3>
+<p>
+Our Service allows you to post, link, store, share and otherwise make available certain information, text, graphics, videos, or other material ("Content"). You are responsible for the …
+
+The Content section is for businesses that allow users to create, edit, share, make content on their websites or apps. For the full disclosure section, create your own Terms and Conditions.
+</p>
+<h3>
+Links To Other Web Sites
+</h3>
+<p>
+Our Service may contain links to third-party web sites or services that are not owned or controlled by Delta Management System.
+
+Delta Management System has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that Delta Management System shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services.
+</p>
+<h3>
+Changes
+</h3>
+<p>
+We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will try to provide at least 30  days' notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion.
+</p>
+<h3>
+Contact Us
+</h3>
+<p>
+If you have any questions about these Terms, please contact us 
+<br><br>
+123 Montreal, Quebec, CA.<br> Phone: +1 1234567890 .<br> "mail@mail.com" .
+</p>
+    
+  </div>
+       
+    </div>
+</div>
+
+<?php 
+    include_once("includes/footer.php");
+?>
+
+
+
+
+
+
+
